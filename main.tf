e091a95077212df91ed962d8591d302576545b8e
vasooty <47421689+vasooty@users.noreply.github.com>
2025-07-31 22:51:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket" "my_gcs_bucket" {
+  name          = "annular-strata-459207-g1" # Replace with a globally unique name
+  location      = "US-CENTRAL1"
+  storage_class = "STANDARD"
+}
