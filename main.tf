32f8074225df518d7e3172e3f86470df21dee6d6
vasooty <47421689+vasooty@users.noreply.github.com>
2025-07-31 22:55:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my_bucket" {
-  name          = github-demo-vas" # Replace with your desired unique bucket name
+  name          = "github-demo-vas" # Replace with your desired unique bucket name
   project       = "annular-strata-459207-g1"         # Replace with your Google Cloud Project ID
   location      = "US-CENTRAL1"             # Choose an appropriate location (e.g., US-CENTRAL1, EUROPE-WEST1)
   force_destroy = true                      # Allows deletion of non-empty buckets
